e7b85b6cd891f3b33efa144ea86672aca62e742c
Gerry Paul <gpaul12@nc.rr.com>
2023-02-16 18:32:15 -0500

    finished highscore js

--- highScores.js
+++ highScores.js
@@ -10,3 +10,14 @@ clear.addEventListener("click", function() {
 var allScores =localStorage.getItem("allScores");
 allScores = JSON.parse(allScores);
 
+if (allScores !== null) {
+    for (var i = 0; i < allScores.length; i++) {
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + "" + allScores[i].score;
+        highScore.appendChild(createLi);
+    }
+};
+
+goBack.addEventListener("click", function() {
+    window.location.replace("./index.html");
+});
